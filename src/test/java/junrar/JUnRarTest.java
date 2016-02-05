160482702bd8c3b054261947192002c3b40713e7
Albertus82 <albertus82@gmail.com>
2016-02-05 21:06:04 +0100

    Format.

--- src/test/java/junrar/JUnRarTest.java
+++ src/test/java/junrar/JUnRarTest.java
@@ -1,11 +1,20 @@
 /*
  * Copyright (c) 2007 innoSysTec (R) GmbH, Germany. All rights reserved.
- * Original author: EW Creation date: 26.09.2007 Source: $HeadURL$ Last changed:
- * $LastChangedDate$ the unrar licence applies to all junrar source and binary
- * distributions you are not allowed to use this source to re-create the RAR
- * compression algorithm Here some html entities which can be used for escaping
- * javadoc tags: "&": "&#038;" or "&amp;" "<": "&#060;" or "&lt;" ">": "&#062;"
- * or "&gt;" "@": "&#064;"
+ * Original author: EW
+ * Creation date: 26.09.2007
+ *
+ * Source: $HeadURL$
+ * Last changed: $LastChangedDate$
+ *
+ *
+ * the unrar licence applies to all junrar source and binary distributions
+ * you are not allowed to use this source to re-create the RAR compression algorithm
+ *
+ * Here some html entities which can be used for escaping javadoc tags:
+ * "&":  "&#038;" or "&amp;"
+ * "<":  "&#060;" or "&lt;"
+ * ">":  "&#062;" or "&gt;"
+ * "@":  "&#064;"
  */
 package junrar;
 
